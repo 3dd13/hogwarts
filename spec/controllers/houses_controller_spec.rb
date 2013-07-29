4be272eafa0cb8307adf8d823a7b705a5dd1aeeb
Eddie Lau <hi@3dd13.me>
2013-07-30 00:32:11 +0800

    get house#show should success

--- spec/controllers/houses_controller_spec.rb
+++ spec/controllers/houses_controller_spec.rb
@@ -1,26 +1,39 @@
 require 'spec_helper'
 
 describe HousesController do
-  it "returns http success" do
-    get 'index'
-    response.should be_success
-  end
-
-  context "when there is no house created" do
-    it "should assigns empty array to @houses" do
+  describe "GET 'index" do
+    it "returns http success" do
       get 'index'
-      assigns(:houses).should eq []
+      response.should be_success
+    end
+
+    context "when there is no house created" do
+      it "should assigns empty array to @houses" do
+        get 'index'
+        assigns(:houses).should eq []
+      end
+    end
+
+    context "when there is one house created" do
+      before(:each) do
+        @house = FactoryGirl.create(:house)
+      end
+
+      it "should assigns @houses" do
+        get 'index'
+        assigns(:houses).should eq [@house]
+      end
     end
   end
 
-  context "when there is one house created" do
+  describe "GET 'show" do
     before(:each) do
       @house = FactoryGirl.create(:house)
     end
 
-    it "should assigns @houses" do
-      get 'index'
-      assigns(:houses).should eq [@house]
+    it "returns http success" do
+      get 'show', id: @house.to_param
+      response.should be_success
     end
   end
 end
