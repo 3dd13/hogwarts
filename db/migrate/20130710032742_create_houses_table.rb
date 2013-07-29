46f624674b8b9e757fe55b47b1eab9a5ed78a904
Eddie Lau <hi@3dd13.me>
2013-07-29 23:43:25 +0800

    added TODO regarding potential problems in migration script

--- db/migrate/20130710032742_create_houses_table.rb
+++ db/migrate/20130710032742_create_houses_table.rb
@@ -1,5 +1,7 @@
 class CreateHousesTable < ActiveRecord::Migration
   def up
+    # TODO wanna do many to many ?
+    # TODO missing index
     create_table :houses do |t|
       t.string :name
       t.references :students
